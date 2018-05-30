8d108c4158e3e4ba53e9555915bbb37cabf22f34
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-05-30 15:16:52 -0400

    Solution for nested components

--- app/helpers/components/component_helper.rb
+++ app/helpers/components/component_helper.rb
@@ -1,27 +1,27 @@
 module Components
   module ComponentHelper
-    def method_missing(method_name, *arguments, &block)
-      if @_component
-        @_component.send(method_name, *arguments, &block)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(method_name, include_private = false)
-      @_component && @_component.respond_to?(method_name) || super
-    end
-
     def component(name, attrs = {})
       yield attrs if block_given?
 
-      @_component = "#{name}_component".classify.constantize.new(attrs)
+      component = "#{name}_component".classify.constantize.new(attrs)
 
-      render(
-        partial: "#{name}/#{name}"
+      view_class = Class.new(SimpleDelegator) do
+        define_method :method_missing do |method_name, *arguments, &block|
+          if component.respond_to?(method_name)
+            component.send(method_name, *arguments, &block)
+          else
+            super(method_name, *arguments, &block)
+          end
+        end
+
+        define_method :respond_to_missing? do |method_name, include_private = false|
+          component.respond_to?(method_name) || super(method_name, include_private)
+        end
+      end
+
+      view_renderer.render(
+        view_class.new(self), partial: "#{name}/#{name}"
       )
-    ensure
-      @_component = nil
     end
   end
 end
