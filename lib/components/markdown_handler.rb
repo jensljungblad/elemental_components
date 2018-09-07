35cac5c2bf4da012de6aa89d834202e55dc54e91
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-09-07 12:56:55 +0200

    Process markdown before erb

--- lib/components/markdown_handler.rb
+++ lib/components/markdown_handler.rb
@@ -2,29 +2,18 @@ module Components
   class MarkdownHandler
     class << self
       def call(template)
-        # source = erb.call(template)
-
-        # <<-SOURCE
-        #   #{name}.render(begin;#{source};end).html_safe
-        # SOURCE
-
-        # TODO: what we want here is to first process with markdown, then with erb
-        <<-SOURCE
-          #{name}.render(begin;'#{template.source}';end).html_safe
-        SOURCE
-      end
-
-      def render(template)
-        markdown.render(template)
+        erb_handler.erb_implementation.new(
+          markdown_renderer.render(template.source)
+        ).src
       end
 
       private
 
-      def erb
+      def erb_handler
         @erb ||= ActionView::Template.registered_template_handler(:erb)
       end
 
-      def markdown
+      def markdown_renderer
         @markdown ||= Redcarpet::Markdown.new(
           Components::MarkdownRenderer,
           autolink: true,
