1dab66b77931ebc57910634910479744cead1253
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-09-12 10:35:16 +0200

    Manually specify width and height

--- app/components/styleguide/example_component.rb
+++ app/components/styleguide/example_component.rb
@@ -1,6 +1,9 @@
 module Styleguide
   class ExampleComponent < Components::Component
-    has_one :example
+    has_one :example do
+      attribute :width
+      attribute :height
+    end
     has_one :example_source
 
     def iframe_id
--- lib/components/markdown_renderer.rb
+++ lib/components/markdown_renderer.rb
@@ -2,11 +2,26 @@ module Components
   class MarkdownRenderer < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
 
+    def parse_attrs(code)
+      pieces = code.split('---')
+
+      if pieces.length > 1
+        attrs = pieces[0].split("\n").map { |i| i.split(': ') }.to_h
+        [attrs, pieces[1]]
+      else
+        attrs = {}
+        [attrs, pieces[0]]
+      end
+    end
+
     def block_code(code, language)
       if language == 'example'
+        attrs, code = parse_attrs(code)
+
         <<-EXAMPLE
           <%= component "styleguide/example" do |c| %>
-            <% c.example do %>
+            <% c.example(width: #{attrs['width'] || 'nil'},
+                         height: #{attrs['height'] || 'nil'}) do %>
               #{code}
             <% end %>
             <% c.example_source do %>
