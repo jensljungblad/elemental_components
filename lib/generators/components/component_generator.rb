284819d61def85dc527e3049c0dda7247fe355ec
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-09-12 14:52:48 +0200

    Fix generator tests

--- lib/generators/components/component_generator.rb
+++ lib/generators/components/component_generator.rb
@@ -25,16 +25,20 @@ module Components
       create_file "app/components/#{name}/#{name}.js"
     end
 
-    # # TODO: check for presence of styleguide/components folder?
-    # def create_md_file
-    #   return if options['skip_md']
-    #
-    #   # this should not use components.pages_path either...
-    #   components_folder = Dir.new(Components.pages_path).find do |file|
-    #     file =~ /[0-9]*_?components/
-    #   end
-    #
-    #   template 'component.md.erb', "app/views/styleguide/#{components_folder}/#{name}.md"
-    # end
+    def create_md_file
+      return if options['skip_md']
+
+      styleguide_path = File.join(destination_root, 'app/views/styleguide')
+
+      raise 'No views/styleguide directory found' unless Dir.exist?(styleguide_path)
+
+      components_path = Dir.new(styleguide_path).find do |file|
+        file =~ /[0-9]*_?components/
+      end
+
+      raise 'No views/styleguide/components directory found' unless components_path
+
+      template 'component.md.erb', File.join(styleguide_path, components_path, "#{name}.md")
+    end
   end
 end
--- test/component_generator_test.rb
+++ test/component_generator_test.rb
@@ -1,19 +1,31 @@
-# require 'test_helper'
-# require_relative '../lib/generators/components/component_generator'
-#
-# class ComponentGeneratorTest < Rails::Generators::TestCase
-#   tests Components::ComponentGenerator
-#   destination File.expand_path('../tmp', __dir__)
-#   setup :prepare_destination
-#
-#   test 'component generator' do
-#     run_generator %w(foobar)
-#     assert_file 'app/components/foobar_component.rb'
-#     assert_file 'app/components/foobar/_foobar.html.erb'
-#     assert_file 'app/components/foobar/foobar.css'
-#     assert_file 'app/components/foobar/foobar.js'
-#
-#     # TODO: need to put folder there first, simulating install generator
-#     # assert_file 'app/views/styleguide/04_components/foobar.md' # TODO: not 04
-#   end
-# end
+require 'test_helper'
+require_relative '../lib/generators/components/component_generator'
+
+class ComponentGeneratorTest < Rails::Generators::TestCase
+  tests Components::ComponentGenerator
+  destination File.expand_path('../tmp', __dir__)
+  setup :prepare_destination
+
+  test 'component generator' do
+    system 'mkdir', '-p', "#{destination_root}/app/views/styleguide/components"
+    run_generator %w(foobar)
+    assert_file 'app/components/foobar_component.rb'
+    assert_file 'app/components/foobar/_foobar.html.erb'
+    assert_file 'app/components/foobar/foobar.css'
+    assert_file 'app/components/foobar/foobar.js'
+    assert_file 'app/views/styleguide/components/foobar.md'
+  end
+
+  test 'component generator when no style guide directory' do
+    assert_raises do
+      run_generator %w(foobar)
+    end
+  end
+
+  test 'component generator when no components directory' do
+    system 'mkdir', '-p', "#{destination_root}/app/views/styleguide"
+    assert_raises do
+      run_generator %w(foobar)
+    end
+  end
+end
