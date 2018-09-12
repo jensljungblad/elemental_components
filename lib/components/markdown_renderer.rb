fe0a9bc3e5a7b3465ab7c1078e4cd0de947bbdf9
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-09-12 10:42:22 +0200

    Refactor example renderer

--- lib/components/markdown_renderer.rb
+++ lib/components/markdown_renderer.rb
@@ -2,6 +2,32 @@ module Components
   class MarkdownRenderer < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
 
+    def block_code(code, language)
+      case language
+      when 'example'
+        attrs, code = parse_attrs(code)
+        example(code, attrs)
+      else
+        super
+      end
+    end
+
+    def example(code, attrs)
+      <<-EXAMPLE
+        <%= component "styleguide/example" do |c| %>
+          <% c.example(width: #{attrs['width'] || 'nil'},
+                       height: #{attrs['height'] || 'nil'}) do %>
+            #{code}
+          <% end %>
+          <% c.example_source do %>
+            #{block_code(code, 'erb')}
+          <% end %>
+        <% end %>
+      EXAMPLE
+    end
+
+    private
+
     def parse_attrs(code)
       pieces = code.split('---')
 
@@ -11,26 +37,6 @@ module Components
       else
         attrs = {}
         [attrs, pieces[0]]
-      end
-    end
-
-    def block_code(code, language)
-      if language == 'example'
-        attrs, code = parse_attrs(code)
-
-        <<-EXAMPLE
-          <%= component "styleguide/example" do |c| %>
-            <% c.example(width: #{attrs['width'] || 'nil'},
-                         height: #{attrs['height'] || 'nil'}) do %>
-              #{code}
-            <% end %>
-            <% c.example_source do %>
-              #{super(code, 'erb')}
-            <% end %>
-          <% end %>
-        EXAMPLE
-      else
-        super
       end
     end
   end
