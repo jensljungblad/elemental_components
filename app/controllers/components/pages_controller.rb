c53b4fbc6953117f1901874f7c2eb2ed1fc73028
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-09-12 15:16:29 +0200

    Working start page

--- app/controllers/components/pages_controller.rb
+++ app/controllers/components/pages_controller.rb
@@ -1,6 +1,9 @@
 module Components
   class PagesController < ApplicationController
     def show
+      unless params[:path]
+        params[:path] = Components.page_names[0][0]
+      end
       render "styleguide/#{params[:path]}"
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Components::Engine.routes.draw do
+  root to: 'pages#show'
   get '*path', to: 'pages#show', as: :page
 end
