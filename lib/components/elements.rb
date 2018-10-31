9ef99571bef1c905b75bc4a3afc0fd26463bc6a3
Jens Ljungblad <jens.ljungblad@gmail.com>
2018-10-31 18:12:08 +0100

    Remove variable

--- lib/components/elements.rb
+++ lib/components/elements.rb
@@ -2,31 +2,38 @@ module Components
   module Elements
     extend ActiveSupport::Concern
 
-    # rubocop:disable Naming/PredicateName
     class_methods do
+      # rubocop:disable Naming/PredicateName
       def has_one(name, &config)
         define_method(name) do |attributes = nil, &block|
           return get_element(name) unless attributes || block
 
-          element_class = config ? Class.new(Element, &config) : Class.new(Element)
-          element = element_class.new(@view, attributes, &block)
+          element_class = config ? Class.new(Element, &config) : Element
 
-          set_element(name, element)
+          set_element(
+            name,
+            element_class.new(@view, attributes, &block)
+          )
         end
       end
+      # rubocop:enable Naming/PredicateName
 
+      # rubocop:disable Naming/PredicateName
       def has_many(name, &config)
         define_method(name) do |attributes = nil, &block|
           return get_element(name, collection: true) unless attributes || block
 
-          element_class = config ? Class.new(Element, &config) : Class.new(Element)
-          element = element_class.new(@view, attributes, &block)
+          element_class = config ? Class.new(Element, &config) : Element
 
-          set_element(name, element, collection: true)
+          set_element(
+            name,
+            element_class.new(@view, attributes, &block),
+            collection: true
+          )
         end
       end
+      # rubocop:enable Naming/PredicateName
     end
-    # rubocop:enable Naming/PredicateName
 
     private
 
