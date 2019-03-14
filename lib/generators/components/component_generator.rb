73f59f330d617485420791271504fbf002a94f31
Brandon Mathis <brandon@imathis.com>
2019-03-14 13:08:16 -0500

    More appeasement of Rubocop

--- lib/generators/components/component_generator.rb
+++ lib/generators/components/component_generator.rb
@@ -5,7 +5,7 @@ module Components
     class_option :skip_css, type: :boolean, default: false
     class_option :skip_js, type: :boolean, default: false
 
-    source_root File.expand_path("../templates", __FILE__)
+    source_root File.expand_path("templates", __dir__)
 
     def create_component_file
       template "component.rb.erb", "app/components/#{name}_component.rb"
@@ -13,23 +13,26 @@ module Components
 
     def create_erb_file
       return if options["skip_erb"]
+
       create_file "app/components/#{name}/_#{filename}.html.erb"
     end
 
     def create_css_file
       return if options["skip_css"]
+
       create_file "app/components/#{name}/#{filename}.css"
     end
 
     def create_js_file
       return if options["skip_js"]
+
       create_file "app/components/#{name}/#{filename}.js"
     end
 
     private
 
     def filename
-      name.split('/').last
+      name.split("/").last
     end
   end
 end
