4a409ce9ca760001ed5562ca09bc3df803e85f9f
Jens Ljungblad <jens.ljungblad@gmail.com>
2019-12-05 19:28:40 -0500

    Rename initializers

--- lib/elemental_components/engine.rb
+++ lib/elemental_components/engine.rb
@@ -4,17 +4,17 @@ module ElementalComponents
   class Engine < ::Rails::Engine
     isolate_namespace ElementalComponents
 
-    initializer "components.asset_paths" do |app|
+    initializer "elemental_components.asset_paths" do |app|
       app.config.assets.paths << ElementalComponents.components_path if app.config.respond_to?(:assets)
     end
 
-    initializer "components.view_helpers" do
+    initializer "elemental_components.view_helpers" do
       ActiveSupport.on_load :action_controller do
         helper ElementalComponents::ComponentHelper
       end
     end
 
-    initializer "components.view_paths" do
+    initializer "elemental_components.view_paths" do
       ActiveSupport.on_load :action_controller do
         append_view_path ElementalComponents.components_path
       end
